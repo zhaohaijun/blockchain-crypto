ea2f23ac7994ccbc21bd5d6d236f871d64befd59
zhaohaijun <eshezhao@gmail.com>
2018-10-17 10:06:54 +0800

    refactor

--- sm3/sm3.go
+++ sm3/sm3.go
@@ -4,10 +4,14 @@ import (
     "hash"
 )
 
-const Size=32
-const BlockSize=64
-const(
-    chunk=64
+// The size of a SM3 checksum in bytes.
+const Size = 32
+
+// The blocksize of SM3 in bytes.
+const BlockSize = 64
+
+const (
+    chunk = 64
     init0 = 0x7380166F
     init1 = 0x4914B2B9
     init2 = 0x172442D7
@@ -17,12 +21,15 @@ const(
     init6 = 0xE38DEE4D
     init7 = 0xB0FB0E4E
 )
-type digest struct{
-    h [8]uint32
-    x [chunk]byte
-    nx int
+
+// digest represents the partial evaluation of a checksum.
+type digest struct {
+    h   [8]uint32
+    x   [chunk]byte
+    nx  int
     len uint64
 }
+
 func (d *digest) Reset() {
     d.h[0] = init0
     d.h[1] = init1
@@ -36,66 +43,70 @@ func (d *digest) Reset() {
     d.len = 0
 }
 
-func New() hash.Hash{
+// New returns a new hash.Hash computing the SM3 checksum.
+func New() hash.Hash {
     d := new(digest)
     d.Reset()
     return d
 }
 
-func (d *digest) Size() int{return Size}
-func(d *digest) BlockSize() int {
-    return BlockSize
-}
-func (d *digest) Write(p []byte)(nn int ,err error){
-    nn=len(p)
-    d.len+=uint64(nn)
-    if d.nx>0{
-        n :=copy(d.x[d.nx:],p)
-        d.nx+=n
-        if d.nx==chunk{
-            block(d,d.x[:])
-            d.nx=0
-        }
+func (d *digest) Size() int { return Size }
 
+func (d *digest) BlockSize() int { return BlockSize }
+
+func (d *digest) Write(p []byte) (nn int, err error) {
+    nn = len(p)
+    d.len += uint64(nn)
+    if d.nx > 0 {
+        n := copy(d.x[d.nx:], p)
+        d.nx += n
+        if d.nx == chunk {
+            block(d, d.x[:])
+            d.nx = 0
+        }
+        p = p[n:]
     }
-    if len(p) >= chunk{
-        n :=len(p) &^(chunk-1)
-        block(d,p[:])
-        p=p[n:]
+    if len(p) >= chunk {
+        n := len(p) &^ (chunk - 1)
+        block(d, p[:n])
+        p = p[n:]
     }
-    if len(p)>0{
-        d.nx=copy(d.x[:],p)
+    if len(p) > 0 {
+        d.nx = copy(d.x[:], p)
     }
     return
 }
-func (d0 *digest) Sum(in []byte) []byte{
-    d:=*d0
-    hash :=d.checkSum()
-    return append(in,hash[:]...)
 
+func (d0 *digest) Sum(in []byte) []byte {
+    // Make a copy of d0 so that caller can keep writing and summing.
+    d := *d0
+    hash := d.checkSum()
+    return append(in, hash[:]...)
 }
 
-func (d0 *digest) checkSum() [Size]byte{
-   len:=d0.len
-   var tmp[64]byte
-   tmp[0]=0x80
-   if len%64<56{
-       d0.Write(tmp[0:56-len%64])
-   }else{
-       d0.Write(tmp[0:64+56-len%64])
-   }
+func (d *digest) checkSum() [Size]byte {
+    len := d.len
+    // Padding. Add a 1 bit and 0 bits until 56 bytes mod 64.
+    var tmp [64]byte
+    tmp[0] = 0x80
+    if len%64 < 56 {
+        d.Write(tmp[0 : 56-len%64])
+    } else {
+        d.Write(tmp[0 : 64+56-len%64])
+    }
+
     // Length in bits.
     len <<= 3
     for i := uint(0); i < 8; i++ {
         tmp[i] = byte(len >> (56 - 8*i))
     }
-    d0.Write(tmp[0:8])
+    d.Write(tmp[0:8])
 
-    if d0.nx != 0 {
+    if d.nx != 0 {
         panic("d.nx != 0")
     }
 
-    h := d0.h[:]
+    h := d.h[:]
 
     var digest [Size]byte
     for i, s := range h {
@@ -106,8 +117,9 @@ func (d0 *digest) checkSum() [Size]byte{
     }
 
     return digest
-
 }
+
+// Sum returns the SM3 checksum of the data.
 func Sum(data []byte) [Size]byte {
     var d digest
     d.Reset()
